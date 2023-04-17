de3ecff3ed73acaefc3f6c0f56732cba9df92d92
= <=>
2023-04-17 21:31:52 +0300

    Deleted conversion functions for now

--- ytdl.py
+++ ytdl.py
@@ -3,7 +3,6 @@ import os
 import sys
 from tkinter import filedialog, Tk
 
-from moviepy.editor import VideoFileClip
 from pytube import YouTube
 
 
@@ -17,43 +16,23 @@ def Download(link, path):
     """
     try:
         youtubeObject = YouTube(link)
-        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
         print(f"Downloading {youtubeObject.title}...")
-        youtubeObject.download(output_path=path)
+        return youtubeObject.download(output_path=path)
     except:
         print("An error has occurred")
     print("Download is completed successfully")
-    return youtubeObject.title + ".mp4"
-
-
-def ConvertMP4toMP3(filepath):
-    """
-    Converts the video file at the specified filepath from MP4 format to MP3 format using moviepy.
-    Returns the filename of the converted audio file.
-
-    filepath : path of the .mp4 file
-    """
-    print(f"Converting {filepath} to .mp3")
-    video_file = VideoFileClip(filepath)
-    audio_file = video_file.audio
-    mp3_filename = filepath.replace(".mp4", ".mp3")
-    audio_file.write_audiofile(mp3_filename)
-    video_file.close()
-    audio_file.close()
-    return mp3_filename
 
 
 # hide the main window
 root = Tk()
 root.withdraw()
-
 # create the parser for command-line arguments
 parser = argparse.ArgumentParser(
     description="Download and convert YouTube videos to MP3",
     epilog="Example usage: python youtube_downloader.py https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-
 # add the arguments
 parser.add_argument("url", metavar="URL", help="the YouTube video URL")
 parser.add_argument(
@@ -62,26 +41,23 @@ parser.add_argument(
     metavar="PATH",
     help="the output directory. If not specified, a file dialog will be shown.",
 )
-
 # parse the arguments
 args = parser.parse_args()
-
 # check if the user requested help
 if "help" in args:
     parser.print_help()
     sys.exit(0)
-
 # get the YouTube URL and output directory
 link = args.url
 filepath = args.output
-
 if not filepath:
     filepath = filedialog.askdirectory()
+file = Download(link, filepath)
+base, ext = os.path.splitext(file)
+new_file = base + '.mp3'
+os.rename(file, new_file)
+print(f"Conversion completed successfully. MP3 file saved at {new_file}")
 
-mp4_filename = Download(link, filepath)
-mp4_filepath = os.path.join(filepath, mp4_filename)
 
-mp3_filename = ConvertMP4toMP3(mp4_filepath)
-os.remove(mp4_filepath)
 
-print(f"Conversion completed successfully. MP3 file saved at {mp3_filename}")
+
