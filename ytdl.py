e99587789ef07716249b860ffdbee544fe17e612
= <=>
2023-04-17 11:35:38 +0300

    Added command line arguments. Changed filename

--- ytdl.py
+++ ytdl.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import sys
 from tkinter import filedialog, Tk
 
 from moviepy.editor import VideoFileClip
@@ -30,8 +32,27 @@ def ConvertMP4toMP3(filepath):
 root = Tk()
 root.withdraw()
 
-link = input("Enter the YouTube video URL: ")
-filepath = filedialog.askdirectory()
+# create the parser for command-line arguments
+parser = argparse.ArgumentParser(description="Download and convert YouTube videos to MP3")
+
+# add the arguments
+parser.add_argument("url", metavar="URL", help="the YouTube video URL")
+parser.add_argument("-o", "--output", metavar="PATH", help="the output directory")
+
+# parse the arguments
+args = parser.parse_args()
+
+# check if the user requested help
+if "help" in args:
+    parser.print_help()
+    sys.exit(0)
+
+# get the YouTube URL and output directory
+link = args.url
+filepath = args.output
+
+if not filepath:
+    filepath = filedialog.askdirectory()
 
 mp4_filename = Download(link, filepath)
 mp4_filepath = os.path.join(filepath, mp4_filename)
